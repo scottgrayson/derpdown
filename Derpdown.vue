641413b799c84f2d7d61b592a604ca0c00944584
Scott Grayson <dscottgrayson@gmail.com>
2016-12-18 11:04:48 -0500

    create Derpdown.vue

---
+++ Derpdown.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="derpdown">
+        <div @click="toggle">
+            <slot name="toggle"></slot>
+        </div>
+        <div class="derpdown-content" v-show="show"
+            :class="{ 'derpdown-right' : align_right }">
+            <slot name="content"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                show: false
+            }
+        },
+        props: {
+            align_right: {
+                type: Boolean,
+                default: false
+            },
+        },
+        methods: {
+            toggle() {
+                this.show = !this.show
+            },
+        }
+    }
+</script>
+
+<style>
+.derpdown-content {
+    position: absolute;
+}
+.derpdown {
+    position: relative;
+}
+.derpdown-right {
+    right: 0;
+}
+</style>
